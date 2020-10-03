dde42f61bad712c4b59ce51a56b7c754e7261ced
porvil <porvil17304@iiitd.ac.in>
2020-10-04 01:37:06 +0530

    Base Layout Added with DrawerLayout

--- app/src/main/java/com/iiitd/dsavisualizer/algorithms/sorting/merge/MergeSortActivity.java
+++ app/src/main/java/com/iiitd/dsavisualizer/algorithms/sorting/merge/MergeSortActivity.java
@@ -1,15 +1,146 @@
 package com.iiitd.dsavisualizer.algorithms.sorting.merge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.iiitd.dsavisualizer.R;
 
+
 public class MergeSortActivity extends AppCompatActivity {
+
+    DrawerLayout dl_main;
+    View v_main;
+    ViewStub vs_main;
+    LinearLayout ll_menu;
+    LinearLayout ll_code;
+    Button btn_backward;
+    Button btn_forward;
+    TextView tv_seqno;
+    FloatingActionButton fab_menubutton;
+
+    SeekBar seekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_merge_sort);
+        setContentView(R.layout.layout_base);
+
+        seekBar = findViewById(R.id.seekBar);
+        dl_main = findViewById(R.id.dl_main);
+        vs_main = findViewById(R.id.vs_main);
+        vs_main.setLayoutResource(R.layout.activity_merge_sort);
+        v_main = vs_main.inflate();
+
+        ll_menu = v_main.findViewById(R.id.ll_menu);
+        ll_code = v_main.findViewById(R.id.ll_code);
+        btn_backward = v_main.findViewById(R.id.btn_backward);
+        btn_forward = v_main.findViewById(R.id.btn_forward);
+        tv_seqno = v_main.findViewById(R.id.tv_seqno);
+        fab_menubutton = v_main.findViewById(R.id.fab_menubutton);
+
+
+        btn_backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("backward");
+            }
+        });
+
+        btn_forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("forward");
+            }
+        });
+
+        fab_menubutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!dl_main.isOpen()) {
+                    dl_main.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+        dl_main.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset){}
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView){}
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView){}
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                if (newState == DrawerLayout.STATE_SETTLING) {
+                    if (!dl_main.isDrawerOpen(Gravity.RIGHT)) {
+                        // Drawer started opening
+                        fab_menubutton.setVisibility(View.INVISIBLE);
+                    } else {
+                        // Drawer started closing
+                        fab_menubutton.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        seekBar.setOnTouchListener(new SeekBar.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow Drawer to intercept touch events.
+                        // v.getParent().requestDisallowInterceptTouchEvent(true);
+                        dl_main.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        // Allow Drawer to intercept touch events.
+                        // v.getParent().requestDisallowInterceptTouchEvent(false);
+                        dl_main.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED);
+                        break;
+                }
+
+                // Handle SeekBar touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                System.out.println(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
