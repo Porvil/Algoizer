a5c2167991bea470dbecb5f55db39a8e13f776c4
porvil <porvil17304@iiitd.ac.in>
2020-12-12 22:30:19 +0530

    Updated UI

--- app/src/main/java/com/iiitd/dsavisualizer/runapp/activities/HomeActivity.java
+++ app/src/main/java/com/iiitd/dsavisualizer/runapp/activities/HomeActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.iiitd.dsavisualizer.R;
 import com.iiitd.dsavisualizer.algorithms.sorting.quick.QuickSortActivity;
@@ -18,6 +20,8 @@ public class HomeActivity extends AppCompatActivity {
     Button btn_about;
 
     Context context;
+
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,4 +56,23 @@ public class HomeActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
 }
